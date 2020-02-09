fe4953cea076bb2c37863904ed2ab3840664b49b
Arthurobdfv <38329059+Arthurobdfv@users.noreply.github.com>
2020-02-09 22:17:05 +0100

    Adding Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentationComponent
   ],
   imports: [
     BrowserModule
