7c025c972fe5ed4596914faf6f697671dd986b2c
Arthurobdfv <38329059+Arthurobdfv@users.noreply.github.com>
2020-02-20 17:52:57 +0100

    Added presentation component and started development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PresentationComponent } from './presentation/presentation.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, PresentationComponent],
 })
 export class AppModule { }
---
+++ src/app/presentation/presentation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+
+import { MatSliderModule } from '@angular/material/slider';
+
+@Component({
+  selector: 'app-presentation',
+  templateUrl: './presentation.component.html',
+  styleUrls: ['./presentation.component.scss']
+})
+export class PresentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
