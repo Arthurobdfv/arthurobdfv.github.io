314eeb0868158e56879f7b7c2dcc1448edb94eff
Arthurobdfv <38329059+Arthurobdfv@users.noreply.github.com>
2020-03-22 11:33:03 +0100

    "UI and Build changes"
    - Added command to build and deploy to github pages on package.json
    - Changes on Presentation component view
    - Changed the primary color of the custom theme from lightblue to indigo
    - Added the correct images for the project mock

--- src/mock-projects.ts
+++ src/mock-projects.ts
@@ -1,7 +1,7 @@
 import { Project } from './models';
 
 export var ProjectList:Project[] = [
-    {title: "Checkers 3D",description:"3D Game Prototype.",imageUrl:"https://arthuro-verissimo.itch.io/3d-checkers-prototype"},
+    {title: "Checkers 3D",description:"3D Game Prototype.",imageUrl:"https://img.itch.zone/aW1nLzMwNDMyODIucG5n/315x250%23c/iqb8LL.png"},
     {title: "NeonSlash",description:"24hours challenge that i've turned into a full project.",imageUrl:"https://img.itch.zone/aW1nLzMxMjAwMzcucG5n/original/jXgJ8V.png"},
     {title: "EPM TagPort",description:"Software that i've developed while working for Elipse Software"}
 ]
